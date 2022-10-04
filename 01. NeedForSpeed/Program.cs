bf98b967e1447b7d7a4dc8c54b91453c88a9d0ca
denkaty <82085828+denkaty@users.noreply.github.com>
2022-10-04 16:51:09 +0300

    Add files via upload

---
+++ 01. NeedForSpeed/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed = 5;
+            int batteryDrain = 2;
+            RemoteControlCar car = new RemoteControlCar(speed, batteryDrain);
+
+            int distance = 800;
+            RaceTrack raceTrack = new RaceTrack(distance);
+
+            car.Drive();
+            bool isBatteryDrained = car.BatteryDrained();
+            Console.WriteLine(isBatteryDrained);
+
+            car = RemoteControlCar.Nitro();
+            car.Drive();
+            int distanceDriven = car.DistanceDriven();
+            Console.WriteLine(distanceDriven);
+
+            bool isTrackFinished = raceTrack.TryFinishTrack(car);
+            Console.WriteLine(isTrackFinished);
+        }
+    }
+    public class RemoteControlCar
+    {
+        // TODO: define the constructor for the 'RemoteControlCar' class
+
+        private int speed;
+        private int batteryDrain;
+
+        private int distanceDrived;
+        private int remainingBatteryPercent;
+
+        public RemoteControlCar(int speed, int batteryDrain)
+        {
+            this.speed = speed;
+            this.batteryDrain = batteryDrain;
+
+            this.distanceDrived = 0;
+            this.remainingBatteryPercent = 100;
+        }
+        public bool BatteryDrained()
+        {
+
+            bool batteryDrain = false;
+            if (this.remainingBatteryPercent <= 0)
+            {
+                batteryDrain = true;
+            }
+            return batteryDrain;
+
+
+            throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        }
+
+        public int DistanceDriven()
+        {
+            return this.distanceDrived;
+            throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+
+        }
+
+        public void Drive()
+        {
+            try
+            {
+                int batteryLeft = this.remainingBatteryPercent - this.batteryDrain;
+                if (batteryLeft > 0)
+                {
+                    this.distanceDrived += this.speed;
+                    this.remainingBatteryPercent = batteryLeft;
+                }
+                else
+                {
+                    Console.WriteLine("battery is drained");
+                }
+            }
+            catch
+            {
+                throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+
+            }
+        }
+
+        public static RemoteControlCar Nitro()
+        {
+            int nitroSpeed = 50;
+            int nitroBatteryDrain = 4;
+            return new RemoteControlCar(nitroSpeed, nitroBatteryDrain);
+            throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        }
+
+        public int getSpeed()
+        {
+            return this.speed;
+        }
+
+        public int getBatteryDrain()
+        {
+            return this.batteryDrain;
+        }
+
+        public int getDistanceDrived()
+        {
+            return this.distanceDrived;
+        }
+
+        public int getRemainingBatteryPercent()
+        {
+            return this.remainingBatteryPercent;
+        }
+    }
+
+    public class RaceTrack
+    {
+        // TODO: define the constructor for the 'RaceTrack' class
+
+        private int distance;
+
+        public RaceTrack(int distance)
+        {
+            this.distance = distance;
+        }
+
+        public bool TryFinishTrack(RemoteControlCar car)
+        {
+            int remainingBatteryPercent = car.getRemainingBatteryPercent();
+            int batteryDrain = car.getBatteryDrain();
+            int distanceDrived = car.getDistanceDrived();
+            int speed = car.getSpeed();
+
+            bool finishedTrack = false;
+            while (!finishedTrack)
+            {
+                if (remainingBatteryPercent > 0)
+                {
+                    remainingBatteryPercent -= batteryDrain;
+                    distanceDrived += speed;
+
+                    if (distanceDrived >= this.distance)
+                    {
+                        finishedTrack = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return finishedTrack;
+
+            throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        }
+    }
+}
---
+++ 02. SpaceAge/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace SpaceAge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int ageInSeconds = int.Parse(Console.ReadLine());
+
+            SpaceAge spaceAge = new SpaceAge(ageInSeconds);
+
+            double ageOnEarth = spaceAge.OnEarth();
+            Console.WriteLine(Math.Round(ageOnEarth, 2));
+
+            double ageOnMercury = spaceAge.OnMercury();
+            Console.WriteLine(Math.Round(ageOnMercury, 2));
+
+            double ageOnVenus = spaceAge.OnVenus();
+            Console.WriteLine(Math.Round(ageOnVenus, 2));
+
+            double ageOnMars = spaceAge.OnMars();
+            Console.WriteLine(Math.Round(ageOnMars, 2));
+
+            double ageOnJupiter = spaceAge.OnJupiter();
+            Console.WriteLine(Math.Round(ageOnJupiter, 2));
+
+            double ageOnSaturn = spaceAge.OnSaturn();
+            Console.WriteLine(Math.Round(ageOnSaturn, 2));
+
+            double ageOnUranus = spaceAge.OnUranus();
+            Console.WriteLine(Math.Round(ageOnUranus, 2));
+
+            double ageOnNeptune = spaceAge.OnNeptune();
+            Console.WriteLine(Math.Round(ageOnNeptune, 2));
+        }
+    }
+    public class SpaceAge
+    {
+        private int ageInSeconds;
+        public SpaceAge(int seconds)
+        {
+            this.ageInSeconds = seconds;
+        }
+
+        public double OnEarth()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+
+        }
+
+        public double OnMercury()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 0.2408467;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnVenus()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 0.61519726;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnMars()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 1.8808158;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnJupiter()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 11.862615;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnSaturn()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 29.447498;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnUranus()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 84.016846;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+
+        public double OnNeptune()
+        {
+            double ageInSecondsDouble = this.ageInSeconds * 1.0;
+            double years = ageInSecondsDouble / 31557600 / 164.79132;
+            return years;
+            throw new NotImplementedException("Implement this function.");
+        }
+    }
+}
